0b5d03c7ea0f95b8444414487ceda4955d1515ef
Michael-Arrington <micarrington@teksystems.com>
2024-03-22 15:59:48 -0600

    minor tweaks

--- Sources/IntegratingJS/ExampleView.swift
+++ Sources/IntegratingJS/ExampleView.swift
@@ -2,30 +2,43 @@ import Foundation
 import SwiftUI
 
 private let jsFunc = """
+function makeFullName(first, last) {
+  if (first !== "") {
+    if (last !== "") {
+      return last + ", " + first
+    } else {
+      return first
+    }
+  }
+
+  return last
+}
+
 function reduce(model, action) {
   if (action._name === "load_initial_values") {
-    return {
-      "firstName": "Bob",
-      "lastName": "Appleton",
-      "fullName": model.fullName
-    }
+    model.firstName = "Bob";
+    model.lastName = "Appleton";
   } else if (action._name === "clear_button_tapped") {
-    return {
-      "firstName": "",
-      "lastName": "",
-      "fullName": model.fullName
-    }
+    model.firstName = "";
+    model.lastName = "";
   } else if (action._name === "first_name_field_committed") {
-    return {
-      "firstName": model.firstName,
-      "lastName": model.lastName,
-      "fullName": model.lastName + ", " + model.firstName // or more complex logic
-    }
-  } else {
-    return model;
+    // nothing to do here, just wait for formatting
+  } else if (action._name === "next_button_tapped") {
+    model.isShowingNextScreen = true;
   }
+
+  model.fullName = makeFullName(model.firstName, model.lastName);
+
+  return model;
 }
 """
+
+/*
+ Helpful improvements to make:
+ - Change actions to an Enum with cases
+ - create an Executor type, to keep a single instance of JSContext alive and avoid extra overhead
+    - should also have a generic for Action type
+ */
 
 protocol Action: Encodable {
     var _name: String { get }
@@ -44,36 +57,52 @@ private struct FirstNameFieldCommitted: Action {
     let _name = toSnakeCase(String(describing: Self.self))
 }
 
+private struct NextButtonTapped: Action {
+    let _name = toSnakeCase(String(describing: Self.self))
+}
+
 struct DiscountsView: View {
     struct Model: Codable {
         var firstName = ""
         var lastName = ""
         var fullName = ""
+        var isShowingNextScreen = false
     }
 
     @State var model = Model()
 
     var body: some View {
         Form {
-            Text(model.firstName)
-            Text(model.lastName)
-            Text(model.fullName)
-            TextField("First Name", text: $model.firstName, onCommit: {
-                // defer most state manipulations to the reducer
-                Current.apply(FirstNameFieldCommitted(), to: &model, using: jsFunc)
-            })
-
-            Button("Clear") {
-                // defer most state manipulations to the reducer
-                Current.apply(ClearButtonTapped(), to: &model, using: jsFunc)
+            Section("Read-only") {
+                Text(model.firstName)
+                Text(model.lastName)
+                Text(model.fullName)
             }
-            Button("Load") {
-                // defer most state manipulations to the reducer
-                Current.apply(LoadInitialValues(), to: &model, using: jsFunc)
+
+            Section("Controls") {
+                TextField("First Name", text: $model.firstName, onCommit: {
+                    // defer most state manipulations to the reducer
+                    Current.apply(FirstNameFieldCommitted(), to: &model, using: jsFunc)
+                })
+
+                Button("Clear") {
+                    // defer most state manipulations to the reducer
+                    Current.apply(ClearButtonTapped(), to: &model, using: jsFunc)
+                }
+                Button("Load") {
+                    // defer most state manipulations to the reducer
+                    Current.apply(LoadInitialValues(), to: &model, using: jsFunc)
+                }
+                Button("Next Screen") {
+                    Current.apply(NextButtonTapped(), to: &model, using: jsFunc)
+                }
             }
         }
         .onAppear {
             Current.apply(LoadInitialValues(), to: &model, using: jsFunc)
+        }
+        .sheet(isPresented: $model.isShowingNextScreen) {
+            Text("This is the next screen")
         }
     }
 }
